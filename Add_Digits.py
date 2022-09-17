c396545a51f6ff76e742ca223c72ff8c882d72b8
sitaram-cr <81864667+sitaram-cr@users.noreply.github.com>
2022-09-17 14:36:22 +0530

    def digsum(n):     sum=0     while(n>0 or sum>9):         if(n==0):             n=sum             sum=0         sum+=n%10         n//=10     return sum n=int(input()) print(digsum(n))

---
+++ Add_Digits.py
@@ -0,0 +1,11 @@
+def digsum(n):
+    sum=0
+    while(n>0 or sum>9):
+        if(n==0):
+            n=sum
+            sum=0 
+        sum+=n%10
+        n//=10
+    return sum
+n=int(input())
+print(digsum(n))
